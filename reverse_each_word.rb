fcb662f986836edc34a298a56902d569f12f2b32
Alice <agenkin1@gmail.com>
2020-06-18 22:01:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
-def reverse_each_word(string)
+
+def reverse_each_word(str) #takes a string
 newArray = []
-  splitString = string.split
+  splitString = str.split #makes string into array
+
+  for each splitString
+  puts newArray.push(splitString)
+
 end
+
