07a473e1939d2f8e5be99cf48bac9ec3ead4892f
Alice <agenkin1@gmail.com>
2020-06-18 22:07:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,5 @@ def reverse_each_word(str) #takes a string
 newArray = []
   splitString = str.split #makes string into array
 splitString.each do |word|
-  new_words = []
-  new_words.push(word.reverse)
-  newArray.push(splitString)
+  word.reverse
 end
